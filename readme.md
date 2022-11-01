f8f8e67988c8f8dcedb171b856a6b19ab9c9221c
egyiptomi425 <tomi04252002@gmail.com>
2022-11-01 18:11:20 +0100

    s

--- readme.md
+++ readme.md
@@ -11,3 +11,15 @@ python -m pip install requests
 ```
 
 ![Konzolos letöltés](./media/requestletoltes.png)
+
+### Program használata
+-------------------
+
+Lépések:
+1. Nyissuk meg a bemenet.txt fájlt
+2. Első sorba írjuk be a youtube videó linkjét, majd hagyjunk ki egy sort
+3. Második sortól írjuk be, hogy melyik *perc*:*másodperc* pontnál kezdődik egy adott fejezet
+
+> Egy fejezet megadása kétféleképpen lehetséges: 1.: Csak az időt írjuk be *perc*:*másodperc* formátumban. 2.: Megadjuk először az időt, majd a írunk egy szóközt, majd egy "-" kötőjelet, majd megint egy szóközt, és a fejezet nevét megadjuk. Példa: "22:47 - Telepités utáni lépések". A percek és a másodpercek számának nem kell két jegyűnek lenni, de legalább egy jegyűnek igen (akkor is, ha 0, pl.: 0:4). Minden sorban csak egy ilyen metaadat szerepelhet. A fileban csak a 2. sor lehet üres.
+
+4. Indítsuk el a timestamp.py fájlt.
--- timestamp.py
+++ timestamp.py
@@ -1,20 +1,29 @@
+from base64 import decode
 from cgitb import html
+from encodings.utf_8 import encode
+from pickle import TRUE
 import webbrowser
 import os
+import requests
 
 def main():
-    ##kimenet=open("kimenet.html", "w")
-    ##kimenet.write()
-    ##kimenet.close()
-    webbrowser.open(os.path.dirname(__file__) + "\media\kimenet.html")
-    a=webbrowser.get("https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=lr3y5deXNQU&format=json")
-    print(a)
-
-
-    url = 'https://www.facebook.com/favicon.ico'
-    r = requests.get(url, allow_redirects=True)
-
-    open('facebook.ico', 'wb').write(r.content)
+    bemenet=open("bemenet.txt", "r", encoding='utf8')
+    url=bemenet.readline()
+    bemenet.readline() #empthy line
+    
+    r = requests.get("https://www.youtube.com/oembed?url=" + url, allow_redirects=True)
+    open(os.path.dirname(__file__) + "\media\kimenet.json", 'wb').write(r.content)
+    
+    info=[(line[0:int(line.find("-"))-1],line[int(line.find("-"))+2:-1]) for line in bemenet]
+    
+    bemenet.close()
+    
+    links=[]
+    
+    kimenet=open("kimenet.html", "w", encoding='utf8')
+    kimenet.write("")
+    kimenet.close()
+    #webbrowser.open(os.path.dirname(__file__) + "\kimenet.html")
 
 if __name__=="__main__":
     main()
